25531b05587ec01af3e4f43583c37c0cb795d4f4
Oscar Wang <oscarw6926@gmail.com>
2024-03-04 10:23:47 -0800

    Game manager setup

---
+++ Assets/Scripts/GameState.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+///  The different states that the game can be in
+/// </summary>
+public enum GameState
+{
+    Menu,
+    Online,
+    Local,
+    Play,
+    GameOver
+}
