91e406b3ae8aa623d1fc7512e9ae06b652322cca
hk <hboo@ucdavis.edu>
2024-03-04 02:24:36 -0800

    Added features to change backgrounds
    Wrote small codes to change backgrounds. Currently have three different backgrounds (pink, green, purple)

--- Assets/Scripts/SkyboxSetter.cs
+++ Assets/Scripts/SkyboxSetter.cs
@@ -2,17 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Skybox))]
 public class SkyboxSetter : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] List<Material> _skyboxMaterials;
+
+    Skybox _skybox;
+
+    void Awake()
     {
-        
+        _skybox = GetComponent<Skybox>();
     }
 
-    // Update is called once per frame
-    void Update()
+    void OnEnable()
     {
-        
+        ChagneSkybox(skyBox:0);
+    }
+
+    void ChagneSkybox(int skyBox)
+    {
+        if (_skybox != null && skyBox >= 0 && skyBox <= _skyboxMaterials.Count)
+        {
+            _skybox.material = _skyboxMaterials[skyBox];
+        }
     }
 }
+
