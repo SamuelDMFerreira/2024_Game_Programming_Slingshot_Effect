7161baabc34b8029a62dfbfe0ba3ac781c640b0b
Eric Xu <xydxydxyd1@gmail.com>
2024-03-14 03:46:18 -0700

    Add movement player input

---
+++ Assets/Scripts/PlayerInputListener.cs
@@ -0,0 +1,60 @@
+/// <summary>
+/// Listens for input actions, most likely sent from PlayerInput
+/// component, and handles them properly; Converts input events
+/// into controller calls.
+/// </summary>
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+[RequireComponent(typeof(PlayerController))]
+[RequireComponent(typeof(ProjectileController))]
+public class PlayerInputListener : MonoBehaviour
+{
+    [SerializeField] PlayerController playerCtrl;
+    [SerializeField] ProjectileController bulletCtrl;
+
+    // Controls
+    private bool isThrusting = false;
+    private bool isBoosting = false;
+    private float turnFactor = 0.0f;
+
+    void Start()
+    {
+        playerCtrl = GetComponent<PlayerController>();
+        bulletCtrl = GetComponent<ProjectileController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isThrusting)
+            playerCtrl.ApplySmallThrust();
+        if (isBoosting)
+            playerCtrl.ApplyEscapeThrust();
+        playerCtrl.HandleInputRotation(turnFactor);
+    }
+
+    public void OnThrust(InputValue inputVal)
+    {
+        isThrusting = inputVal.isPressed;
+    }
+
+    public void OnBoost(InputValue inputVal)
+    {
+        isBoosting = inputVal.isPressed;
+    }
+
+    public void OnTurn(InputValue inputVal)
+    {
+        Debug.Log("Turning!");
+        turnFactor = inputVal.Get<float>();
+    }
+
+    public void OnFire()
+    {
+        Debug.Log("FIRE!");
+    }
+}
