013ecc4b46d8fd4f1a3586c9fe279f3fec95091f
Oscar <oscarw6926@gmail.com>
2024-03-14 02:40:42 -0700

    updated file name

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class HealthController : MonoBehaviour
+public class PlayerHealth : MonoBehaviour
 {
     public float maxHealth;
     public float currentHealth;
