363926d118574e0639f44cec5cc837d6c953ea1d
Eric Xu <xydxydxyd1@gmail.com>
2024-03-21 01:05:09 -0700

    Update end menu with flavor text and background

--- Assets/Scripts/EndMenu.cs
+++ Assets/Scripts/EndMenu.cs
@@ -6,6 +6,13 @@ using UnityEngine;
 public class EndMenu : MonoBehaviour
 {
     [SerializeField] private TMP_Text endText;
+    private static List<string> victoryTexts = new List<string>{
+        " secured another victory for the galactic empire!",
+        " spread managed democracy throughout the universe!",
+        " can't wait to go back to watch some anime.",
+        ", ten years from now, will go down in history as the greatest pilot of the era",
+        " turned around for a selfie",
+    };
 
     // Start is called before the first frame update
     void Awake()
@@ -15,6 +22,9 @@ public class EndMenu : MonoBehaviour
 
     void Start()
     {
+        int winner = GameManager.instance.Winner;
+        string victoryText = victoryTexts[Random.Range(0, victoryTexts.Count - 1)];
+        endText.text = $"Player {winner}" + victoryText;
     }
 
     void OnDestroy()
